815e8c51cdcc2067fcdc29db352c8bbede4043ff
Temur Radjabov <t.radjabov@andersenlab.com>
2022-06-14 19:16:29 +0300

    fixed

--- cats/src/index.js
+++ cats/src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter >
     <App />
   </HashRouter>
 );
